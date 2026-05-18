ddd6bc1390ed2ec905375194fb2022d3dc099b52
waltersingh024 <Waltersingh032@gmail.com>
2026-05-17 22:43:04 -0400

    Neon CHange

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@
 import { neon } from '@neondatabase/serverless';
 
 // Connecting using your clean project credentials directly
-const sql = neon("postgresql://neondb_owner:npg_6UorJjeMhqi3@ep-wild-bread-aq20vhfn-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require");
+const sql = neon("postgresql://neondb_owner:npg_Ut6bKvS0hJgW@ep-sweet-dream-an3lgaii-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
 
 export async function load() {
   // Pulls your database rows and explicitly converts the date format 
